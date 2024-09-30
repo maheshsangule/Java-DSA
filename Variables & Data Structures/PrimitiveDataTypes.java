b283d4bff9c71c2c9af73b3353858a4e01988cae
Mahesh Sangule <maheshsangule7@gmail.com>
2024-09-30 22:36:47 +0530

    P 30-09-2024

---
+++ Variables & Data Structures/PrimitiveDataTypes.java
@@ -0,0 +1,24 @@
+
+public class PrimitiveDataTypes {
+
+    public static void main(String[] args) {
+
+        byte b = 10;
+        System.out.println(b);
+        short sh = 2000;
+        System.out.println(sh);
+        char ch = 'A';
+        System.out.println(ch);
+        boolean bl = true;
+        System.out.println(bl);
+        int num = 8;
+        System.out.println(num);
+        long l = 1111111111;
+        System.out.println(l);
+        float price = (float) 15.0;
+        System.out.println(price);
+        double rate = 1111111111111.5;
+        System.out.println(rate);
+    }
+
+}
